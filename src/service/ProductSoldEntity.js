e9a39db7369dac4aca291884a7304c38bac00f0c
Chen Jiadong <coolboy096188@gmail.com>
2017-03-04 14:27:37 +0900

    On branch develop [add] 追加了ProductSoldEntity的Constructor和get，set函数

--- src/service/ProductSoldEntity.js
+++ src/service/ProductSoldEntity.js
@@ -41,10 +41,20 @@ var ProductSoldEntity = (function() {
     //クラス内定数
 
     //コンストラクタ
-    var ProductSoldEntity = function() {
+    var ProductSoldEntity = function(recordID, lastUpdateTime, accountSettlementMonth, productName, itemCount, itemUnitPriceJPY, deliveryChargeJPY, itemRetailPriceCNY, deliveryChargeCNY, isPaid) {
         if (!(this instanceof ProductSoldEntity)) {
-            return new ProductSoldEntity();
+            return new ProductSoldEntity(recordID, lastUpdateTime, accountSettlementMonth, productName, itemCount, itemUnitPriceJPY, deliveryChargeJPY, itemRetailPriceCNY, deliveryChargeCNY, isPaid);
         }
+        this.recordID = recordID;
+        this.lastUpdateTime = lastUpdateTime;
+        this.accountSettlementMonth = accountSettlementMonth;
+        this.productName = productName;
+        this.itemCount = itemCount;
+        this.itemUnitPriceJPY = itemUnitPriceJPY;
+        this.deliveryChargeJPY = deliveryChargeJPY;
+        this.itemRetailPriceCNY = itemRetailPriceCNY;
+        this.deliveryChargeCNY = deliveryChargeCNY;
+        this.isPaid = isPaid;
         this.init();
     }
 
@@ -70,11 +80,65 @@ var ProductSoldEntity = (function() {
     p.putToDatastore = function() {
         throw new Error('Not Implemented');
     }
+    p.getRecordID = function() {
+        return this.recordID;
+    }
+    p.setRecordID = function(recordID) {
+        this.recordID = recordID;
+    }
     p.getLastUpdateTime = function() {
         return this.lastUpdateTime;
     }
     p.setLastUpdateTime = function(lastUpdateTime) {
         this.lastUpdateTime = lastUpdateTime;
+    }
+    p.getAccountSettlementMonth = function() {
+        return this.accountSettlementMonth;
+    }
+    p.setAccountSettlementMonth = function(accountSettlementMonth) {
+        this.accountSettlementMonth = accountSettlementMonth;
+    }
+    p.getProductName = function() {
+        return this.productName;
+    }
+    p.setProductName = function(productName) {
+        this.productName = productName;
+    }
+    p.getItemCount = function() {
+        return this.itemCount;
+    }
+    p.setItemCount = function(itemCount) {
+        this.itemCount = itemCount;
+    }
+    p.getItemUnitPriceJPY = function() {
+        return this.itemUnitPriceJPY;
+    }
+    p.setItemUnitPriceJPY = function(itemUnitPriceJPY) {
+        this.itemUnitPriceJPY = itemUnitPriceJPY;
+    }
+    p.getDeliveryChargeJPY = function() {
+        return this.deliveryChargeJPY;
+    }
+    p.setDeliveryChargeJPY = function(deliveryChargeJPY) {
+        this.deliveryChargeJPY = deliveryChargeJPY;
+    }
+    p.getItemRetailPriceCny = function() {
+        return this.itemRetailPriceCNY;
+    }
+    p.setItemRetailPriceCny = function(itemRetailPriceCNY) {
+        this.itemRetailPriceCNY = itemRetailPriceCNY;
+    }
+    p.getDeliveryChargeCNY = function() {
+        return this.deliveryChargeCNY
+    }
+    p.setDeliveryChargeCNY = function(deliveryChargeCNY) {
+        this.deliveryChargeCNY = deliveryChargeCNY;
+    }
+    p.getIsPaid = function() {
+        return this.isPaid;
+    }
+    p.setIsPaid = function(isPaid) {
+        this.isPaid = isPaid;
     }
     return ProductSoldEntity;
 })();
