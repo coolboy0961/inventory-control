7c6910e832086233595c5f267ebd0ff2decc12a2
Chen Jiadong <coolboy096188@gmail.com>
2017-03-05 22:17:17 +0900

    [add]实现了对所有未结算商品进行结算，包括以前的结算月为结算的商品
    modified:   src/service/ProductSoldsAggregateEntity.js

--- src/service/ProductSoldsAggregateEntity.js
+++ src/service/ProductSoldsAggregateEntity.js
@@ -56,6 +56,9 @@ var ProductSoldsAggregateEntity = (function() {
     p.findAllProductSolds = function() {
         return this.productSoldRepository.valuesToEntities();
     }
+    p.findAllPaymentSettlement = function() {
+        return this.paymentSettlementRepository.valuesToEntities();
+    }
     p.putAll = function() {
         throw new Error('Not Implemented');
     }
@@ -69,28 +72,46 @@ var ProductSoldsAggregateEntity = (function() {
     p.accountSettlement = function() {
         var exchange = this.config.getExchange();
         var productSolds = this.findAllProductSolds();
-        var paymentSettlement = new PaymentSettlementEntity();
-        var haveTarget = false;
+        var paymentSettlements = this.findAllPaymentSettlement();
+        var targetAccountSettlementMonths = new Object();
+        //从未结算的已售出商品中确定需要结算的月份
         for (var key in productSolds) {
             if (productSolds.hasOwnProperty(key)) {
-                if (Utility.isEmpty(productSolds[key].getAccountSettlementMonth()) && Utility.formatDateToMonth(productSolds[key].getLastUpdateTime()) == Utility.formatDateToMonth(new Date()) && productSolds[key].getIsPaid() == "否") {
-                    paymentSettlement.setCostAmountJPY(paymentSettlement.getCostAmountJPY() + (productSolds[key].getItemCount() * productSolds[key].getItemUnitPriceJPY()));
-                    paymentSettlement.setCostAmountJPY(paymentSettlement.getCostAmountJPY() + (productSolds[key].getDeliveryChargeJPY()));
-                    paymentSettlement.setCostAmountJPY(paymentSettlement.getCostAmountJPY() + (productSolds[key].getDeliveryChargeCNY() * exchange));
-                    paymentSettlement.setSalesAmountJPY(paymentSettlement.getSalesAmountJPY() + (productSolds[key].getItemCount() * (productSolds[key].getItemRetailPriceCNY() * exchange)));
-                    haveTarget = true;
+                if (Utility.isEmpty(productSolds[key].getAccountSettlementMonth()) && Utility.formatDateToMonth(productSolds[key].getLastUpdateTime()) == Utility.formatDateToMonth(new Date()) && productSolds[key].getIsPaid() == "是") {
+                    targetAccountSettlementMonths[Utility.formatDateToMonth(productSolds[key].getLastUpdateTime())] = Utility.formatDateToMonth(productSolds[key].getLastUpdateTime());
                 }
             }
         }
-        if (haveTarget) {
-            paymentSettlement.setIncomeAmountJPY(paymentSettlement.getSalesAmountJPY() - paymentSettlement.getCostAmountJPY());
-            paymentSettlement.setCostAmountCNY(paymentSettlement.getCostAmountJPY() / exchange);
-            paymentSettlement.setIncomeAmountCNY(paymentSettlement.getIncomeAmountJPY() / exchange);
-            paymentSettlement.setSalesAmountCNY(paymentSettlement.getSalesAmountJPY() / exchange);
-            paymentSettlement.setExchange(exchange);
-            paymentSettlement.put();
-            paymentSettlement.putToDatastore();
+        //为每个对象月进行结算
+        for (var accountSettlementMonth in targetAccountSettlementMonths) {
+            if (targetAccountSettlementMonths.hasOwnProperty(accountSettlementMonth)) {
+                var paymentSettlement;
+                if (Utility.isEmpty(paymentSettlements[accountSettlementMonth])) {
+                    paymentSettlement = new PaymentSettlementEntity();
+                } else {
+                    paymentSettlement = paymentSettlements[accountSettlementMonth];
+                }
+                for (var recordID in productSolds) {
+                    if (productSolds.hasOwnProperty(recordID)) {
+                        if (Utility.isEmpty(productSolds[recordID].getAccountSettlementMonth()) && Utility.formatDateToMonth(productSolds[recordID].getLastUpdateTime()) == accountSettlementMonth && productSolds[recordID].getIsPaid() == "是") {
+                            paymentSettlement.setCostAmountJPY(paymentSettlement.getCostAmountJPY() + (productSolds[recordID].getItemCount() * productSolds[recordID].getItemUnitPriceJPY()));
+                            paymentSettlement.setCostAmountJPY(paymentSettlement.getCostAmountJPY() + (productSolds[recordID].getDeliveryChargeJPY()));
+                            paymentSettlement.setCostAmountJPY(paymentSettlement.getCostAmountJPY() + (productSolds[recordID].getDeliveryChargeCNY() * exchange));
+                            paymentSettlement.setSalesAmountJPY(paymentSettlement.getSalesAmountJPY() + (productSolds[recordID].getItemCount() * (productSolds[recordID].getItemRetailPriceCNY() * exchange)));
+                            productSolds[recordID].setAccountSettlementMonth(paymentSettlement.getAccountSettlementMonth());
+                            productSolds[recordID].put();
+                        }
+                    }
+                }
+                paymentSettlement.setIncomeAmountJPY(paymentSettlement.getSalesAmountJPY() - paymentSettlement.getCostAmountJPY());
+                paymentSettlement.setCostAmountCNY(paymentSettlement.getCostAmountJPY() / exchange);
+                paymentSettlement.setIncomeAmountCNY(paymentSettlement.getIncomeAmountJPY() / exchange);
+                paymentSettlement.setSalesAmountCNY(paymentSettlement.getSalesAmountJPY() / exchange);
+                paymentSettlement.setExchange(exchange);
+                paymentSettlement.put();
+            }
         }
+        this.putToDatastore();
     }
     return ProductSoldsAggregateEntity;
 })();
